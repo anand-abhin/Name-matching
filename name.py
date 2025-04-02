f123ef7ae2c271e93c884df3e9376cbe40c978bb
Abhinav Anand <abhinav.anand_int@acko.tech>
2025-04-02 12:31:13 +0530

    implemented different algorithm fuzzwuzz

--- name.py
+++ name.py
@@ -1,6 +1,7 @@
 from collections import Counter
 from difflib import SequenceMatcher
 import re
+from thefuzz import fuzz
 
 def preprocess(name):
     name = name.lower().replace(".", "")
@@ -22,20 +23,20 @@ def ngram_similarity(name1, name2, n=2):
     counter2 = Counter(ngrams2)
     intersection = sum((counter1 & counter2).values())
     total = sum((counter1 | counter2).values())
-
     return intersection / total if total > 0 else 0
 
 # Case 1: Use of abbreviations 
 name1 = "Mr. Ravi Kumar"
 name2 = "RaviKumar"
 similarity_score = ngram_similarity(name1, name2, n=2)
-print(f"Case 1.1: {similarity_score:.2f}")
+ratio = fuzz.ratio(preprocess(name1), preprocess(name2))
+print(f"Case 1.1: n-grams:{similarity_score:.2f}, fuzz score: {ratio:.2f}")
 
 name1 = "Mrs. Vijayalakshmi M. N."
 name2 = "Miss Vijayalakshmi"
 similarity_score = ngram_similarity(name1, name2, n=2)
-print(f"Case 1.2: {similarity_score:.2f}")
-
+ratio = fuzz.ratio(preprocess(name1), preprocess(name2))
+print(f"Case 1.2: n-grams:{similarity_score:.2f}, fuzz score: {ratio:.2f}")
 
 # Case 2: Name order differences 
 name1 = "Dheeraj Manirathnam Anna"
@@ -47,13 +48,15 @@ print(f"Case 2: {similarity_score:.2f}")
 name1 = "R. K. Narayan"
 name2 = "Rasipuram Krishnaswami Narayan"
 similarity_score = ngram_similarity(name1, name2, n=2)
-print(f"Case 3: {similarity_score:.2f}")
+ratio = fuzz.ratio(preprocess(name1), preprocess(name2))
+print(f"Case 3: n-grams:{similarity_score:.2f}, fuzz score: {ratio:.2f}")
 
 # Case 4: Abbreviated names
 name1 = "Vijayalakshmi"
 name2 = "lakshmi"
 similarity_score = ngram_similarity(name1, name2, n=2)
-print(f"Case 4: {similarity_score:.2f}")
+ratio = fuzz.ratio(preprocess(name1), preprocess(name2))
+print(f"Case 4: n-grams:{similarity_score:.2f}, fuzz score: {ratio:.2f}")
 
 # Case 5: Surname mismatch (partial/full)
 name1 = "Ravi Kumar"
@@ -65,13 +68,15 @@ print(f"Case 5: {similarity_score:.2f}")
 name1 = "Ravi Kumar"
 name2 = "Ravi"
 similarity_score = ngram_similarity(name1, name2, n=2)
-print(f"Case 6: {similarity_score:.2f}")
+ratio = fuzz.ratio(preprocess(name1), preprocess(name2))
+print(f"Case 6: n-grams:{similarity_score:.2f}, fuzz score: {ratio:.2f}")
 
 # Case 7: Spelling variations (typos): 
 name1 = "Omprakash"
 name2 = "Onprakash"
 similarity_score = ngram_similarity(name1, name2, n=2)
-print(f"Case 7: {similarity_score:.2f}")
+ratio = fuzz.ratio(preprocess(name1), preprocess(name2))
+print(f"Case 7: n-grams:{similarity_score:.2f}, fuzz score: {ratio:.2f}")
 
 # Case 8: Missed or extra spaces: 
 name1 = "Ravi Kumar"
@@ -83,10 +88,12 @@ print(f"Case 8: {similarity_score:.2f}")
 name1 = "Ravi Kumar"
 name2 = "Ravi Ravi Kumar"
 similarity_score = ngram_similarity(name1, name2, n=2)
-print(f"Case 9: {similarity_score:.2f}")
+ratio = fuzz.ratio(preprocess(name1), preprocess(name2))
+print(f"Case 9: n-grams:{similarity_score:.2f}, fuzz score: {ratio:.2f}")
 
-# Case 8: Missing part of name: 
+# Case 10: Missing part of name: 
 name1 = "Dheeraj M. Anna" 
 name2 = "Dheeraj Anna"
 similarity_score = ngram_similarity(name1, name2, n=2)
-print(f"Case 8: {similarity_score:.2f}")
+ratio = fuzz.ratio(preprocess(name1), preprocess(name2))
+print(f"Case 10: n-grams:{similarity_score:.2f}, fuzz score: {ratio:.2f}")
